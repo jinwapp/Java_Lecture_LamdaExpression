5ca616671c86608c8a4a0108e7ae6df756118f64
jinwapp <jinwaap@gmail.com>
2022-06-06 23:38:43 +0900

    람다식 표현 예제입니다.

--- src/ReferenceOfInstanceMethod_Type2_1/ReferenceOfInstanceMethod_Type2_1.java
+++ src/ReferenceOfInstanceMethod_Type2_1/ReferenceOfInstanceMethod_Type2_1.java
@@ -8,24 +8,24 @@ class B{
     void bcd(int k){
         System.out.println(k);
     }
-}
+    public class ReferenceOfInstanceMethod_Type2_1 {
+        public static void main(String[] args) {
+            //#인스턴스 메서드 참조 Type 2
+            //@1 익명이너클래스
+            A a1 = new A(){
+                @Override
+                public void abc(B b, int k) {
+                    // 객체 b가 만들어져서 매개변수 b로 넘어온 케이스기 때문에 객체 b를 만들지 않아도된다.
+                    b.bcd(k);
+                }
+            };
+            //@2 람다식
+            A a2 = (B b, int k)->b.bcd(k);
 
-public class ReferenceOfInstanceMethod_Type2_1 {
-    public static void main(String[] args) {
-        //#인스턴스 메서드 참조 Type 2
-        //@1 익명이너클래스
-        A a1 = new A(){
-            @Override
-            public void abc(B b, int k) {
-                // 객체 b가 만들어져서 매개변수 b로 넘어온 케이스기 때문에 객체 b를 만들지 않아도된다.
-                b.bcd(k);
-            }
-        };
-        //@2 람다식
-        A a2 = (B b, int k)->b.bcd(k);
+            //@3 인스턴스 메서드 참조 Type2
+            A a3 = B::bcd;
 
-        //@2 람다식
-        A a3 = (B b, int k)->b.bcd(k);
-
+        }
     }
+
 }
